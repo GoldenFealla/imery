0567dbfd558ee9b097ae795b00cb67bd2380621e
GoldenFealla <telisacaramore@gmail.com>
2026-03-18 18:01:31 +0700

    fix: restyle rotate

--- src/app/pages/main/pages/edit/components/preview/preview.ts
+++ src/app/pages/main/pages/edit/components/preview/preview.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, input, ViewChild } from '@angular/core';
+
+// Panzoom
+import Panzoom, { PanZoom } from 'panzoom';
 
 // Icons
 import { NgIcon, provideIcons } from '@ng-icons/core';
@@ -15,4 +18,21 @@ import { lucideLoader } from '@ng-icons/lucide';
 export class Preview {
   url = input<string | null>(null);
   isPreviewing = input<boolean>(false);
+
+  private panzoomInstance!: PanZoom;
+  @ViewChild('imageElement') imageElement!: ElementRef<HTMLImageElement>;
+
+  ngAfterViewInit() {
+    const el = this.imageElement?.nativeElement;
+
+    if (el) {
+      this.panzoomInstance = Panzoom(el, {
+        bounds: false,
+      });
+    }
+  }
+
+  ngOnDetroy() {
+    this.panzoomInstance.dispose();
+  }
 }
--- src/app/pages/main/pages/edit/components/side-bar/components/sidebar-rotate.ts
+++ src/app/pages/main/pages/edit/components/side-bar/components/sidebar-rotate.ts
@@ -1,22 +1,30 @@
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
+// Icons
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideRefreshCcw } from '@ng-icons/lucide';
+
+// Spartan
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmSliderImports } from '@spartan-ng/helm/slider';
 @Component({
   selector: 'sidebar-rotate',
-  imports: [],
+  imports: [NgIcon, HlmButtonImports, HlmSliderImports],
+  providers: [provideIcons({ lucideRefreshCcw })],
   template: `
     <section>
       <p class="text-xs font-medium text-muted-foreground mb-2">Rotate</p>
       <div class="flex items-center gap-3">
-        <input
-          type="range"
+        <hlm-slider
           min="-180"
           max="180"
-          step="1"
-          [value]="angle()"
-          (input)="angleChange.emit(+$any($event.target).value)"
-          class="flex-1"
+          [value]="[angle()]"
+          (valueChange)="angleChange.emit($event[0])"
         />
         <span class="text-sm text-muted-foreground w-10 text-right"> {{ angle() }}° </span>
+        <button hlmBtn size="icon" variant="ghost" (click)="reset()">
+          <ng-icon name="lucideRefreshCcw"></ng-icon>
+        </button>
       </div>
     </section>
   `,
@@ -31,4 +39,8 @@ export class SidebarRotate {
   angle = input<number>(0);
 
   angleChange = output<number>();
+
+  reset() {
+    this.angleChange.emit(0);
+  }
 }
