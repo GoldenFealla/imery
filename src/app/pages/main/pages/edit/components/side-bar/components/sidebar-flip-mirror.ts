34b0803c034e639b1edcffafdc6fb314e59cf4d8
GoldenFealla <telisacaramore@gmail.com>
2026-03-23 12:14:21 +0700

    fix: refractor flip mirror

--- src/app/pages/main/pages/edit/components/side-bar/components/sidebar-flip-mirror.ts
+++ src/app/pages/main/pages/edit/components/side-bar/components/sidebar-flip-mirror.ts
@@ -1,29 +1,40 @@
 import { ChangeDetectionStrategy, Component, effect, input, model, output, signal } from '@angular/core';
 
-// Models
-import { TransformOptions } from '@models/image';
-
 // Icons
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideFlipHorizontal, lucideFlipVertical } from '@ng-icons/lucide';
+import { ToggleValue } from '@spartan-ng/brain/toggle-group';
 
 // Spartan
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmToggleGroupImports } from '@spartan-ng/helm/toggle-group';
+
+type State = 'flip' | 'mirror';
 
 @Component({
   selector: 'sidebar-flip-mirror',
-  imports: [NgIcon, HlmButtonImports],
+  imports: [NgIcon, HlmToggleGroupImports, HlmButtonImports],
   providers: [provideIcons({ lucideFlipVertical, lucideFlipHorizontal })],
   template: `
     <section>
       <p class="text-xs font-medium text-muted-foreground mb-2">Flip / Mirror</p>
       <div class="flex gap-2">
-        <button hlmBtn [variant]="flipState() ? 'default' : 'outline'" size="sm" (click)="handleOnFlip()">
-          <ng-icon name="lucideFlipVertical" size="14" class="mr-1" /> Flip
-        </button>
-        <button hlmBtn [variant]="mirrorState() ? 'default' : 'outline'" size="sm" (click)="handleOnMirror()">
-          <ng-icon name="lucideFlipHorizontal" size="14" class="mr-1" /> Mirror
-        </button>
+        <hlm-toggle-group
+          type="multiple"
+          variant="outline"
+          spacing="2"
+          size="sm"
+          [nullable]="true"
+          [value]="default"
+          (valueChange)="handleOnValueChange($event)"
+        >
+          <button hlmToggleGroupItem class="data-[state=on]:bg-primary data-[state=on]:text-background" size="sm" value="flip">
+            <ng-icon name="lucideFlipVertical" size="14" class="mr-1" /> Flip
+          </button>
+          <button hlmToggleGroupItem class="data-[state=on]:bg-primary data-[state=on]:text-background" size="sm" value="mirror">
+            <ng-icon name="lucideFlipHorizontal" size="14" class="mr-1" /> Mirror
+          </button>
+        </hlm-toggle-group>
       </div>
     </section>
   `,
@@ -35,27 +46,14 @@ import { HlmButtonImports } from '@spartan-ng/helm/button';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarFlipMirror {
-  flipState = signal<boolean>(false);
-  mirrorState = signal<boolean>(false);
+  default: State[] = [];
 
   flip = output<boolean>();
   mirror = output<boolean>();
 
-  constructor() {
-    effect(() => {
-      this.flip.emit(this.flipState());
-    });
-
-    effect(() => {
-      this.mirror.emit(this.mirrorState());
-    });
-  }
-
-  handleOnFlip() {
-    this.flipState.set(!this.flipState());
-  }
-
-  handleOnMirror() {
-    this.mirrorState.set(!this.mirrorState());
+  handleOnValueChange(state: ToggleValue<State>) {
+    const states = state as State[];
+    this.flip.emit(states.includes('flip'));
+    this.mirror.emit(states.includes('mirror'));
   }
 }
